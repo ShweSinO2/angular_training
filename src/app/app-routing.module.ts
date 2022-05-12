8e2485ba8804787627e8540ad202a5ed9c493f17
Shwe Sin Oo <shwesinmyat7@gmail.com>
2022-05-12 12:34:44 +0630

    #day18 (commit for assignment day18)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { AssignmentDay12Component } from './pages/assignment-day12/assignment-da
 import { AssignmentDay14Component } from './pages/assignment-day14/assignment-day14.component';
 import { AssignmentDay15Component } from './pages/assignment-day15/assignment-day15.component';
 import { AssignmentDay16Component } from './pages/assignment-day16/assignment-day16.component';
+import { AssignmentDay18Component } from './pages/assignment-day18/assignment-day18.component';
 import { AssignmentDay4Component } from './pages/assignment-day4/assignment-day4.component';
 import { AssignmentDay5Component } from './pages/assignment-day5/assignment-day5.component';
 import { AssignmentDay6Component } from './pages/assignment-day6/assignment-day6.component';
@@ -73,6 +74,10 @@ const routes: Routes = [
   {
     path: 'day16',
     component: AssignmentDay16Component
+  },
+  {
+    path: 'day18',
+    component: AssignmentDay18Component
   }
 ];
 
--- src/app/ng-material/ng-material.module.ts
+++ src/app/ng-material/ng-material.module.ts
@@ -4,13 +4,13 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
     declarations: [],
@@ -26,7 +26,8 @@ import { MatPaginatorModule } from '@angular/material/paginator';
         MatRadioModule,
         MatButtonModule,
         MatSelectModule,
-        MatPaginatorModule
+        MatPaginatorModule,
+        MatSortModule
     ],
     exports: [
         CommonModule,
@@ -40,7 +41,8 @@ import { MatPaginatorModule } from '@angular/material/paginator';
         MatRadioModule,
         MatButtonModule,
         MatSelectModule,
-        MatPaginatorModule
+        MatPaginatorModule,
+        MatSortModule
     ]
 })
 export class NgMaterialModule { 
